96447bc95af64c1cfa9559196906a300c725b298
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-28 20:44:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,12 +8,12 @@ def map(source_array)
   new_array
 end
   
-def reduce(source_array) 
-  new_array = [] 
+def reduce(source_array, starting_value=0) 
   i = 0 
+  total = 0 
   while i < source_array.length do 
-  new_array << yield(source_array[i])
+  total = starting_value + source_array[i] 
   i += 1 
   end
-  new_array
+  total 
 end
